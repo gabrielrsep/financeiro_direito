a2712003025dffea4eb07e307b840e6007c0e706
Gabriel R. Sepulveda <gabrielrsep@hotmail.com>
2026-01-15 19:43:50 -0300

    bugfix: firstInit

--- server/plugins/database.ts
+++ server/plugins/database.ts
@@ -1,23 +1,27 @@
-import { db, firstInit, sqlFolder } from "../database/connection";
-import { readFileSync, existsSync } from "fs";
-import { resolve } from "path";
+import { existsSync, readFileSync } from "fs";
+import { dirname, resolve } from "path";
+
 
 export default defineNitroPlugin(async () => {
+    const { DATABASE_URL } = process.env;
+    const databaseFile = DATABASE_URL!.replace("file:", "")
+    const databasePath = dirname(databaseFile)
+    const firstInit = !existsSync(databaseFile)
+
+    const sqlFolder = resolve(process.cwd(), "server/database/sql")
+
+    const { db } = await import("../database/connection");
+
     if (firstInit && process.env.NODE_ENV === "development") {
         console.log("Initializing database for the first time...");
-        
-        const schemaPath = resolve(sqlFolder, "schema.sql");
-        if (existsSync(schemaPath)) {
-            const schema = readFileSync(schemaPath).toString();
-            await db.execute(schema);
-            console.log("Database schema applied.");
-        }
 
-        const seedPath = resolve(sqlFolder, "seed.sql");
-        if (existsSync(seedPath)) {
-            const seed = readFileSync(seedPath).toString();
-            await db.execute(seed);
-            console.log("Database seed applied.");
-        }
+        const schema = readFileSync(resolve(sqlFolder, "schema.sql")).toString();
+        db.executeMultiple(schema);
+
+        console.log("Database schema applied.");
+
+        const seed = readFileSync(resolve(sqlFolder, "seed.sql")).toString();
+        db.executeMultiple(seed);
+        console.log("Database seed applied.");
     }
 });
