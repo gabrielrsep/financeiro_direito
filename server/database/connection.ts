e42b900a9b8e4f351fafe79886556660ddd4b84b
Gabriel R. Sepulveda <gabrielrsep@hotmail.com>
2026-01-14 22:06:53 -0300

    bugfix: adicionado variável de ambiente para o token do banco de dados

--- server/database/connection.ts
+++ server/database/connection.ts
@@ -2,10 +2,17 @@ import { createClient } from "@libsql/client";
 import { existsSync } from "fs";
 import { resolve } from "path";
 
-const { DATABASE_URL } = process.env;
+const { DATABASE_URL, DATABASE_AUTH_TOKEN, NODE_ENV } = process.env;
+
+let authToken: string | undefined;
+
+if (NODE_ENV === "production") {
+    authToken = DATABASE_AUTH_TOKEN!;
+}
 
 const db = createClient({
     url: DATABASE_URL!,
+    authToken,
 });
 
 const databasePath = DATABASE_URL!.replace("file:", "");
